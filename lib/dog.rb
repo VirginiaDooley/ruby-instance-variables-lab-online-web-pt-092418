96e31f78594a8feb26f0d5bfac9dc9b73fc2c07d
Virginia Dooley <vcdooley@gmail.com>
2018-10-09 08:59:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog 
-  def name = (dog_name)
+  def name = (dog_name) #setting 
     this_dogs_name = dog_name
   end 
   
   def name 
-    this_dogs_name
+    this_dogs_name #getting 
   end 
 end 
